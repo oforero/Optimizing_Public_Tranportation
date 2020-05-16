8526df272128dccaa06ec0329ed61969f6208ddb
Oscar Mauricio Forero Carrillo <oforero@ieee.org>
2020-05-16 15:56:30 +0800

    feat: Complete the code for the Turnstile producer

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -6,20 +6,20 @@ from confluent_kafka import avro
 
 from models.producer import Producer
 from models.turnstile_hardware import TurnstileHardware
+from dataclasses import dataclass, asdict
 
 
 logger = logging.getLogger(__name__)
 
+@dataclass
+class Turnstile:
+    station_id: int = 0
+    station_name: str = ""
+    line: str = ""
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -38,11 +38,11 @@ class Turnstile(Producer):
         #
         #
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            f"turnstile_{station_name}",
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=1,
+            num_replicas=1,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -51,9 +51,14 @@ class Turnstile(Producer):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
         logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
-        # TODO: Complete this function by emitting a message to the turnstile topic for the number
-        # of entries that were calculated
-        #
-        #
+        for _ in range(num_entries):
+            value = asdict(Turnstile(
+                station_id=self.station.station_id,
+                station_name=self.station.name,
+                line=self.station.color.name
+            ))
+            self.producer.produce(
+                topic=self.topic_name,
+                key={"timestamp": int(self.time_millis())},
+                value=value
+            )
